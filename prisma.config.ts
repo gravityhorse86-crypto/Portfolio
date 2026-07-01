05ddc6824ecfb70f835c6ad91a0f0ad7105b2c22
Osuga <gravityhorse86@gmail.com>
2026-07-02 07:47:39 +0900

    deployできないエラーの解消

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,16 +5,16 @@ import { defineConfig } from "prisma/config";
 
 const databaseUrl = process.env["DATABASE_URL"];
 
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set");
-}
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
-  datasource: {
-    url: databaseUrl,
-  },
+  ...(databaseUrl
+    ? {
+        datasource: {
+          url: databaseUrl,
+        },
+      }
+    : {}),
 });
